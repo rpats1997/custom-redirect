0fbd47d09b027e8f867e3b815c3f753edaf3758d
Rahul P <rahul.p@smartenspaces.com>
2020-10-27 05:08:58 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CustomRedirect';
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+  goToUrl(): void {
+    this.document.location.href = 'https://buildmymomentum.com';
+  }
+  ngOnInit(){
+    this.goToUrl();
+  }
 }
